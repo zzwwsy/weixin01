e741df4dc8d82b6d6b6e22e33789c7d70fc49809
Administrator <Administrator@YOTK13UA0HZV7M2>
2019-04-10 16:16:35 +0800

    更新

--- src/main/java/org/zzwwsy/weixin/WeixinApplication.java
+++ src/main/java/org/zzwwsy/weixin/WeixinApplication.java
@@ -2,14 +2,12 @@ package org.zzwwsy.weixin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 
 @SpringBootApplication
 public class WeixinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeixinApplication.class, args);
-	}
+	} 
 
 }
