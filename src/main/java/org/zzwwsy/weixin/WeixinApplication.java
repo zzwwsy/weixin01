3827a58d1b7105749bfb58e5ae92359a6dea1f47
Administrator <Administrator@YOTK13UA0HZV7M2>
2019-04-12 22:13:53 +0800

    接收微信公众号平台发送给我们的消息

--- src/main/java/org/zzwwsy/weixin/WeixinApplication.java
+++ src/main/java/org/zzwwsy/weixin/WeixinApplication.java
@@ -2,7 +2,7 @@ package org.zzwwsy.weixin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class WeixinApplication {
--- src/main/java/org/zzwwsy/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/zzwwsy/weixin/controller/MessageReceiverController.java
@@ -1,7 +1,12 @@
 package org.zzwwsy.weixin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 控制器 : 负责接收用户的请求参数、调用业务逻辑层代码、返回视图/结果给客户端（浏览器）
@@ -15,12 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/weixin01/weixin/receiver")
 public class MessageReceiverController { 
 
+	//日志记录器
+	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
+	
 	@GetMapping // 只处理GET请求
 	public String echo(//
-			String signature, //
-			String timestamp, //
-			String nonce, //
-			String echostr//
+			@RequestParam("signature") String signature, //
+			@RequestParam("timestamp") String timestamp, //
+			@RequestParam("nonce") String nonce, //
+			@RequestParam("echostr") String echostr//
 	) {
 		// 正常来讲，需要把timestamp和nonce放入一个数组，并进行排序
 		// 接着把排序后的两个元素拼接成一个新的String
@@ -30,4 +38,41 @@ public class MessageReceiverController {
 		// 原路返回echostr的值，返回以后微信公众号平台就能够认为：服务器对接成功
 		return echostr;
 	}
+
+	
+		@PostMapping
+		//@RequestBody注解表示把请求内容获取出来，并且转换成string传入给xml参数。
+		public String onMessage(
+				@RequestParam("signature") String signature, //
+				@RequestParam("timestamp") String timestamp, //
+				@RequestParam("nonce") String nonce, //
+				@RequestBody String xml) {
+			//收到消息
+			//{}是占位符，第一个{}会把第二个参数的值自动填入；
+			//LOG.trace必须要求日志记录器的配置为Trace级别才能输出
+			LOG.trace("收到的消息原文：\n{}\n-------------------------------------", xml);
+//			转换消息
+//			把消息丢入队列
+//			消费队列中的消息
+//			产生客服消息
+			
+			return "success";
+		}
+		
+		
 }	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
